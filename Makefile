a722aae314936a9ae582ac566134e69d3241213c
Mark Steve Samson <hello@marksteve.com>
2019-06-02 16:38:18 +0800

    Update pdf layout

--- Makefile
+++ Makefile
@@ -3,6 +3,10 @@
 resume.pdf: resume.md
 	grip resume.md --wide --export - | \
 		python3 format.py | \
-		wkhtmltopdf --title "Resume - Mark Steve Samson" - resume.pdf
+		wkhtmltopdf \
+			-T 1in -B 1in -L 1in -R 1in \
+			-s A4 \
+			--title "Resume - Mark Steve Samson" \
+			- resume.pdf
 clean:
 	rm resume.pdf
--- format.py
+++ format.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     style = soup.new_tag('style')
     style.append('''
         * { font-family: "Liberation Sans", sans-serif !important; }
-        h1, h2, h3, h4 { margin-top: 3rem !important; }
+        h1, h2, h3, h4 { margin-top: 2rem !important; }
         footer { margin-top: 6rem; text-align: right; }
     ''')
     soup.html.head.append(style)
